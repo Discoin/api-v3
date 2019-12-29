b81801cb2faaf0eb8c3a4e2371143649a6e4c79c
Jonah Snider <jonah@jonah.pw>
2019-12-29 12:53:55 -0800

    refactor: stop returning an awaited expression in migration
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/db/migrations/currency-value-type-change.migration.ts
+++ src/db/migrations/currency-value-type-change.migration.ts
@@ -2,10 +2,10 @@ import {MigrationInterface, QueryRunner} from 'typeorm';
 
 export class CurrencyValueTypeChange1577606626735 implements MigrationInterface {
 	async up(queryRunner: QueryRunner): Promise<any> {
-		return await queryRunner.query(`ALTER TABLE "currencies" ALTER COLUMN "value" TYPE NUMERIC`);
+		return queryRunner.query('ALTER TABLE "currencies" ALTER COLUMN "value" TYPE NUMERIC');
 	}
 
 	async down(queryRunner: QueryRunner): Promise<any> {
-		return await queryRunner.query(`ALTER TABLE "currencies" ALTER COLUMN "value" TYPE DOUBLE PRECISION`);
+		return queryRunner.query('ALTER TABLE "currencies" ALTER COLUMN "value" TYPE DOUBLE PRECISION');
 	}
 }
