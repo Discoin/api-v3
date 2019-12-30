f15f9750bf618d1a7835f36fcab575a25bccf3f4
Austin Huang <im@austinhuang.me>
2019-12-30 12:51:45 -0500

    Update conversion-check.guard.ts

--- src/transactions/conversion-check.guard.ts
+++ src/transactions/conversion-check.guard.ts
@@ -1,8 +1,8 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, BadRequestException, ForbiddenException } from '@nestjs/common';
-import { getRepository } from 'typeorm';
-import { SignedInBot } from 'types/bot';
-import { Currency } from 'src/currencies/currency.entity';
-import { Transaction } from './transaction.entity';
+import {Injectable, CanActivate, ExecutionContext, UnauthorizedException, BadRequestException, ForbiddenException} from '@nestjs/common';
+import {getRepository} from 'typeorm';
+import {SignedInBot} from 'types/bot';
+import {Currency} from 'src/currencies/currency.entity';
+import {Transaction} from './transaction.entity';
 
 @Injectable()
 export class ConversionCheckGuard implements CanActivate {
@@ -12,7 +12,7 @@ export class ConversionCheckGuard implements CanActivate {
 			user?: SignedInBot
 		} = context.switchToHttp().getRequest();
 		const signedInBot = req.user;
-		const { body } = req;
+		const {body} = req;
 		const currencies = getRepository(Currency);
 
 		if (!signedInBot) {
@@ -22,18 +22,11 @@ export class ConversionCheckGuard implements CanActivate {
 		if (body) {
 			const toCurrency = await currencies.findOne(body.toId);
 			if (body.toId === signedInBot.currency.id) {
-				throw new BadRequestException(
-					`You can not convert ${signedInBot.currency.id} to ${body.toId} because they are the same`
-				);
+				throw new BadRequestException(`You can not convert ${signedInBot.currency.id} to ${body.toId} because they are the same`);
 			} else if (!toCurrency) {
 				throw new BadRequestException(`Currency ${body.toId} does not exist`);
-			} else if (
-				(body.amount * signedInBot.currency.value) / toCurrency.value >=
-				toCurrency.reserve
-			) {
-				throw new ForbiddenException(
-					`You cannot exhaust the reserve of ${body.toId}`
-				);
+			} else if ((body.amount * signedInBot.currency.value) / toCurrency.value >= toCurrency.reserve) {
+				throw new ForbiddenException(`You cannot exhaust the reserve of ${body.toId}`);
 			} else {
 				return true;
 			}
